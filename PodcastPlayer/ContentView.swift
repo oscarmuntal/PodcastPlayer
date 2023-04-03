36f907744331e4850c47d7c41e679c8daa590b5a
Òscar Muntal <oscar.muntal@gmail.com>
2023-04-03 18:38:03 +0200

    Initial Commit

---
+++ PodcastPlayer/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  PodcastPlayer
+//
+//  Created by Òscar Muntal on 3/4/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ PodcastPlayer/PodcastPlayerApp.swift
@@ -0,0 +1,17 @@
+//
+//  PodcastPlayerApp.swift
+//  PodcastPlayer
+//
+//  Created by Òscar Muntal on 3/4/23.
+//
+
+import SwiftUI
+
+@main
+struct PodcastPlayerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
