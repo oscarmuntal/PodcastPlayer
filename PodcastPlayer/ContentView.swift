e121103158d265e6abbcc587f97fad00ec7a687d
Òscar Muntal <oscar.muntal@gmail.com>
2023-04-03 18:54:05 +0200

    Episode Model and showing it's properties on the screen

--- PodcastPlayer/ContentView.swift
+++ PodcastPlayer/ContentView.swift
@@ -8,11 +8,22 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    let episode = Episode(name: "Macbreak Weekly", track: "WWDC 2019")
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            Text(self.episode.name)
+                .font(.title)
+            
+            Text(self.episode.track)
+                .foregroundColor(.secondary)
+            
+            
+        }
     }
 }
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
---
+++ PodcastPlayer/Models/Episode.swift
@@ -0,0 +1,13 @@
+//
+//  Episode.swift
+//  PodcastPlayer
+//
+//  Created by Òscar Muntal on 3/4/23.
+//
+
+import Foundation
+
+struct Episode {
+    let name: String
+    let track: String
+}
