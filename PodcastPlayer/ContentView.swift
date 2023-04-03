29fe4465391da83b414a62c0102107c84fffe1d0
Òscar Muntal <oscar.muntal@gmail.com>
2023-04-03 19:21:26 +0200

    Binding of the state variable isPlaying with the new PlayButton so when the user presses the button the name's color changes between black and green

--- PodcastPlayer/ContentView.swift
+++ PodcastPlayer/ContentView.swift
@@ -11,19 +11,34 @@ struct ContentView: View {
     
     let episode = Episode(name: "Macbreak Weekly", track: "WWDC 2019")
     
+    @State private var isPlaying = false
+    
     var body: some View {
         VStack {
             Text(self.episode.name)
                 .font(.title)
+                .foregroundColor(self.isPlaying ? .green : .black)
             
             Text(self.episode.track)
                 .foregroundColor(.secondary)
             
-            
+            PlayButton(isPlaying: $isPlaying)
         }
     }
 }
 
+
+struct PlayButton: View {
+    @Binding var isPlaying: Bool
+    
+    var body: some View {
+        Button(action: {
+            isPlaying.toggle()
+        }) {
+            Text("Play")
+        }.padding(12)
+    }
+}
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
